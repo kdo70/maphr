bfcbb9dcfaa31b8a56c4f9a32f00ad7b246710be
kdo <d.krivosheev70@gmail.com>
2023-02-11 18:53:04 +0700

    #000 fix: secure milleware

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -37,6 +37,7 @@ class Kernel extends HttpKernel
             \Illuminate\View\Middleware\ShareErrorsFromSession::class,
             \App\Http\Middleware\VerifyCsrfToken::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
+            SecureMiddleware::class
         ],
 
         'api' => [
@@ -64,6 +65,5 @@ class Kernel extends HttpKernel
         'signed' => \App\Http\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
-        'secure' => SecureMiddleware::class,
     ];
 }
