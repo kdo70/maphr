cf830b35f5b1cc5924192133f924cb6e3f55847b
kdo <d.krivosheev70@gmail.com>
2023-02-13 22:50:41 +0700

    #000 fix: bootstrap config

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -36,10 +36,15 @@ return [
             'secret' => env('PUSHER_APP_SECRET'),
             'app_id' => env('PUSHER_APP_ID'),
             'options' => [
-                'host' => env('PUSHER_HOST') ?: 'api-'.env('PUSHER_APP_CLUSTER', 'mt1').'.pusher.com',
-                'port' => env('PUSHER_PORT', 443),
-                'scheme' => env('PUSHER_SCHEME', 'https'),
+                'cluster' => env('PUSHER_APP_CLUSTER'),
+                'host' => "app",
+                'port' => 49465,
+                'scheme' => 'https',
                 'encrypted' => true,
+                'curl_options' => [
+                    CURLOPT_SSL_VERIFYHOST => 0,
+                    CURLOPT_SSL_VERIFYPEER => 0,
+                ],
                 'useTLS' => env('PUSHER_SCHEME', 'https') === 'https',
             ],
             'client_options' => [
